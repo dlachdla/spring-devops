c1602596af48b95ba12e77b3da9d1c8a23b3e397
dlachdla <chyolim4692@naver.com>
2024-02-05 17:57:54 +0900

    Feat: 프로젝트 생성, prod-db 설정, wallets submodule 추가, gradle build 준비

---
+++ src/main/java/com/sh/app/SpringDevposApplication.java
@@ -0,0 +1,13 @@
+package com.sh.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevposApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDevposApplication.class, args);
+    }
+
+}
